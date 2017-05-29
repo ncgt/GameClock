2a34b49e751544ddd4fb7ef1183ba0737d66cc06
Nuno <reff@live.com.pt>
2017-05-29 11:33:38 +0100

    Version 1.0

--- GameClock/GameClock/GameClock.cs
+++ GameClock/GameClock/GameClock.cs
@@ -1,56 +1,27 @@
 ﻿using GrandTheftMultiplayer.Server.API;
 using System;
 using System.Timers;
-using System.Collections.Generic;
 
 namespace GameClock
 {
     public class GameClock : Script
     {
         static double racio = 4;
-        TimeSpan Clock = new TimeSpan(0, 0, 0); //serverSidedTime (int hours, int minutes, int seconds)
-        TimeSpan AddOnTime = new TimeSpan(0, 0, 1); //need to calculate this value
+        static TimeSpan FullDay = new TimeSpan(24, 0, 0);//How can I simplify this?
+        TimeSpan RClock = new TimeSpan(5, 20, 00); //realTime when shutdown
         Timer updateConsole;
 
         public GameClock() { API.onResourceStart += API_onResourceStart; }
 
-
-
-        public KeyValuePair<TimeSpan, TimeSpan> FindMinute(double racio)
-        {
-            TimeSpan day = new TimeSpan(1, 0, 0, 0);
-            TimeSpan minute = TimeSpan.FromSeconds(60);
-            TimeSpan IntervalToAdd = new TimeSpan(0, 0, 0);
-            TimeSpan TimeToAdd = new TimeSpan(0, 0, 0);
-            //double duration = 60;
-            do
-            {
-                IntervalToAdd = TimeSpan.FromSeconds(((1 * (day.TotalSeconds / racio) / day.TotalSeconds)));
-                TimeToAdd = TimeSpan.FromSeconds(((1 * (day.TotalSeconds) / (day.TotalSeconds / racio))));
-            } while (TimeToAdd <= minute);
-
-            return new KeyValuePair<TimeSpan, TimeSpan>(IntervalToAdd, TimeToAdd);
-        }
+        public void SetTime() {
+            
+            RClock = RClock.Add(TimeSpan.FromSeconds(1));
+            TimeSpan GClock = TimeSpan.FromSeconds((RClock.TotalSeconds * FullDay.TotalSeconds) / (FullDay.TotalSeconds / racio));
+            API.setTime(GClock.Hours, GClock.Minutes);}
 
         private void API_onResourceStart()
-        {
+        {updateConsole = new Timer(){Interval = 1000}; updateConsole.Elapsed += Elapsed_Console_Timer; updateConsole.Enabled = true;}
 
-            API.setTime(Clock.Hours, Clock.Minutes);
-            //API.consoleOutput("" + FindMinute(racio) + "   ");
-            updateConsole = new Timer()
-            {
-                Interval = FindMinute(racio).Key.Milliseconds //milliseconds
-            };
-            updateConsole.Elapsed += Elapsed_Console_Timer; updateConsole.Enabled = true;
-            API.consoleOutput("IntervalToAdd: " + FindMinute(racio).Key + " TimeToAdd: " + FindMinute(racio).Value);
-        }
-
-        private void Elapsed_Console_Timer(object sender, ElapsedEventArgs e)
-        {
-            Clock = Clock.Add(FindMinute(racio).Value);
-            API.setTime(Clock.Hours, Clock.Minutes);
-            API.consoleOutput("G:" + Clock);
-        }
+        private void Elapsed_Console_Timer(object sender, ElapsedEventArgs e){SetTime();}
     }
 }
-//NCUNHA
